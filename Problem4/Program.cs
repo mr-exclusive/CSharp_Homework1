04666c91511098b24d648ca7f9c06395053fd132
alexey.polyanichko <alexey.polyanichko@gmail.com>
2022-11-19 20:24:13 +0200

    Problem 4

---
+++ Problem4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.Write("Enter 1st number: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Enter 2nd number: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Enter 3rd number: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine("max = " + max);
