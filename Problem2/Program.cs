c44c3ff6930af63e75ae05e50d025d840ab0ebf3
alexey.polyanichko <alexey.polyanichko@gmail.com>
2022-11-19 20:11:54 +0200

    Problem 2

---
+++ Problem2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("Enter 1st number: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Enter 2nd number: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("max = " + a);
+}
+else if (a < b)
+{
+    Console.WriteLine("max = " + b);
+}
+else
+{
+     Console.WriteLine("They are equal!");
+}
